893e9d05c62b26ffd700567650e7ca6df6be8881
Natan Siqueira <natan.siqueira@icloud.com>
2023-09-01 19:42:52 -0300

    add 3rd stage

--- app/main.rb
+++ app/main.rb
@@ -18,16 +18,25 @@ when 'cat-file'
   raise "Unkown option #{option}" unless option == '-p'
   raise 'You must provide a hash' if hash.nil?
 
-  blob_dir = hash[0..1]
-  blob_sha = hash[2..]
-  object_path = File.join('.git', 'objects', blob_dir, blob_sha)
+  object_dir = hash[0..1]
+  object_sha = hash[2..]
+  object_path = File.join('.git', 'objects', object_dir, object_sha)
   raise "Not a valid object name #{hash}" unless File.exist? object_path
 
   compressed = File.read(object_path)
   uncompressed = Zlib::Inflate.inflate(compressed)
   _, content = uncompressed.split("\0")
   print content
+when 'hash-object'
+  option = ARGV[1]
+  filepath = ARGV[2]
+  raise 'You must provide an option. Valid option is -w' if option.nil?
+  raise "Unkown option #{option}" unless option == '-w'
+  raise 'You must provide a file name' if filepath.nil?
 
+  content = File.open(filepath).read
+  object = "blob #{content.size}\0#{content}"
+  puts object
 else
   raise "Unknown command #{command}"
 end
