37785d92bcfa2fd9cbed9b82192f74da6ab539ee
Natan Siqueira <natan.siqueira@icloud.com>
2023-09-01 18:23:36 -0300

    add 2nd stage

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'zlib'
+
 command = ARGV[0]
 
 case command
@@ -9,6 +11,22 @@ when 'init'
   Dir.mkdir('.git/refs')
   File.write('.git/HEAD', "ref: refs/heads/master\n")
   puts 'Initialized git directory'
+when 'cat-file'
+  option = ARGV[1]
+  hash = ARGV[2]
+  raise 'You must provide an option. Valid option is -p' if option.nil?
+  raise "Unkown option #{option}" unless option == '-p'
+  raise 'You must provide a hash' if hash.nil?
+
+  blob_dir = hash[0..1]
+  blob_sha = hash[2..]
+  object_path = File.join('.git', 'objects', blob_dir, blob_sha)
+  raise "Not a valid object name #{hash}" unless File.exist? object_path
+
+  compressed = File.read(path)
+  uncompressed = Zlib::Inflate.inflate(compressed)
+  puts uncompressed.split("\0")
+
 else
   raise "Unknown command #{command}"
 end
