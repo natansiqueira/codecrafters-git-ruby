2a9b2ff1f22a172897352a50319a8cf1530c1790
Natan Siqueira <natan.siqueira@icloud.com>
2023-09-07 16:30:13 -0300

    add 5th stage

--- app/main.rb
+++ app/main.rb
@@ -29,7 +29,7 @@ def write_tree(path)
   files.each_with_object(tree_content) do |file|
     object_hash = hash_object file unless File.directory? file 
     file_mode = format('%o', File.stat(file).mode)
-    tree_content << "#{file_mode} #{file}\0 #{object_hash}\n"
+    tree_content.concat "#{file_mode} #{file}\0 #{object_hash}\n"
   end
 
   puts tree_content
