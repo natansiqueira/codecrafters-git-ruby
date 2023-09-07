398fca05007706aa1082f1886e8b2e782464ee4b
Natan Siqueira <natan.siqueira@icloud.com>
2023-09-07 20:43:57 -0300

    add 5th stage

--- app/main.rb
+++ app/main.rb
@@ -95,7 +95,7 @@ when 'ls-tree'
   uncompressed = Zlib::Inflate.inflate(compressed)
   tree_object = uncompressed.split("\0")
   tree_object.each do |tree_child|
-    file = tree_child.scan(/[a-zA-Z]+$/)
+    file = tree_child.scan(/\s[a-zA-Z]+$/)
     puts file
   end
 when 'write-tree'
